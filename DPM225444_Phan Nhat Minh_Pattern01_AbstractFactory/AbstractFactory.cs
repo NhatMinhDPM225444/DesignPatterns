612d747aeeac2863286664a0cb82c9242d2e124a
NhatMinhDPM225444 <minh_dpm225444@student.agu.edu.vn>
2025-09-11 10:43:13 +0700

    Add files via upload

---
+++ DPM225444_Phan Nhat Minh_Pattern01_AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225444_Phan_Nhat_Minh_Pattern01_AbstractFactory
+{
+        public abstract class AbstractFactory
+        {
+            public abstract AbstractProductA CreateProductA();
+            public abstract AbstractProductB CreateProductB();
+        }
+ }
+ 
---
+++ DPM225444_Phan Nhat Minh_Pattern01_AbstractFactory/AbstractProductA.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225444_Phan_Nhat_Minh_Pattern01_AbstractFactory
+{
+    public abstract class AbstractProductA { }
+}
